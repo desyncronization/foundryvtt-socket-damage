18efda3eecca2e27842d35081e0b295fabf6c48e
el_koha <stewqq.phone@gmail.com>
2025-07-10 12:58:24 +0300

    v1.1 remove chat msg, cleanup

--- scripts/handlers.js
+++ scripts/handlers.js
@@ -13,18 +13,11 @@ function doDamage(sourceID, targetID, targetBodyPart,damage) {
     let source = canvas.tokens.get(sourceID);
     let target = canvas.tokens.get(targetID);
 
-    let chatMSG = `<p>${source.document.name} pizdanul po (${targetBodyPart}) ${target.document.name} na ${damage}</p>`;
-
     let resultTargetHP = target.actor.system.props[targetBodyPart] - damage;
     
-    chatMSG += `<p>HP ${target.document.name} (${targetBodyPart}) bilo ${target.actor.system.props[targetBodyPart]}, stanet ${resultTargetHP}</p>`;
-
     let propToUpdate = 'system.'+targetBodyPart
     target.actor.update({propToUpdate:resultTargetHP})
 
-    ChatMessage.create({
-        content: chatMSG,
-    });
     return true;
 }
 
