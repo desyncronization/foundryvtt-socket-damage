f04cad64ba9c9d08a5784bcb2502e6324132148b
el_koha <stewqq.phone@gmail.com>
2025-07-10 13:06:46 +0300

    hotfix

--- scripts/handlers.js
+++ scripts/handlers.js
@@ -3,14 +3,14 @@ let socket;
 Hooks.once("socketlib.ready", () => {
     socket = socketlib.registerModule("koha-socket-damage");
     socket.register("doDamage", doDamage);
+    socket.register("setHP", setHP);
 });
 
 Hooks.once("ready", async () => {
     game.modules.get("koha-socket-damage").sckt=socket
 });
 
-function doDamage(sourceID, targetID, targetBodyPart, damage) {
-    let source = canvas.tokens.get(sourceID);
+function doDamage(sourceID='zaloopa', targetID, targetBodyPart, damage) {
     let target = canvas.tokens.get(targetID);
 
     let resultTargetHP = target.actor.system.props[targetBodyPart] - damage;
