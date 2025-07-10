cff4336406ac142d817f93b2f97fe225859b05df
el_koha <stewqq.phone@gmail.com>
2025-07-10 13:04:41 +0300

    added setHP function

--- scripts/handlers.js
+++ scripts/handlers.js
@@ -9,14 +9,23 @@ Hooks.once("ready", async () => {
     game.modules.get("koha-socket-damage").sckt=socket
 });
 
-function doDamage(sourceID, targetID, targetBodyPart,damage) {
+function doDamage(sourceID, targetID, targetBodyPart, damage) {
     let source = canvas.tokens.get(sourceID);
     let target = canvas.tokens.get(targetID);
 
     let resultTargetHP = target.actor.system.props[targetBodyPart] - damage;
-    
-    let propToUpdate = 'system.'+targetBodyPart
-    target.actor.update({propToUpdate:resultTargetHP})
+
+    let propToUpdate = "system." + targetBodyPart;
+    target.actor.update({ propToUpdate: resultTargetHP });
+
+    return true;
+}
+
+function setHP(targetID, targetBodyPart, finalHP) {
+    let target = canvas.tokens.get(targetID);
+
+    let propToUpdate = "system." + targetBodyPart;
+    target.actor.update({ propToUpdate: finalHP });
 
     return true;
 }
